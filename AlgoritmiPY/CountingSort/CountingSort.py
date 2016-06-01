b58297e0419edbbbb9d18278bb862bbd0e3184dc
PietroMB <PietroMB@users.noreply.github.com>
2016-06-01 20:09:34 +0200

    Update and rename AlgoritmiPY/CountingSort.py to AlgoritmiPY/CountingSort/CountingSort.py
    Added comment to code

--- AlgoritmiPY/CountingSort/CountingSort.py
+++ AlgoritmiPY/CountingSort/CountingSort.py
@@ -1,12 +1,12 @@
 def CountingSort(list, k):
-    CList = [0] * k
-    SList = [0] * len(list)
+    CList = [0] * k #create a list for storing cardinality of a number
+    SList = [0] * len(list) #Sorted list
     for i in range(0, len(list)):
-        CList[list[i]] += 1
+        CList[list[i]] += 1 #Count cardinality of list
     j = 0
     for i in range(0,k):
         while CList[i] > 0:
-            SList[j] = i
+            SList[j] = i #create a sorted list
             j += 1
             CList[i] -= 1
     return SList
